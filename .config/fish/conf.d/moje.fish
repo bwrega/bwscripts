badc75e7756ff9713ec717fd27e2c3d9cfdecaf7
Bartlomiej Wrega <bwrega@gmail.com>
2016-08-24 22:43:36 +0200

    fish - settings specific for szmira

--- .config/fish/conf.d/moje.fish
+++ .config/fish/conf.d/moje.fish
@@ -3,3 +3,8 @@ alias gd "git diff"
 alias POGODA "curl http://wttr.in/warsaw"
 alias man "env LANG=en_US man"
 
+#set -ex GREP_OPTIONS
+
+set -x JREBEL_HOME /home/bartek/Soft/jrebel
+set -x GOPATH /home/bartek/sandbox/go
+set -x GOROOT /home/bartek/Soft/go
