2ab012c2b7419377ef0242a50c034b9a25a60f75
Bartłomiej Wręga <bartlomiej.wrega@kontomatik.com>
2016-08-26 14:28:50 +0200

    fish - added Knt

--- home/.config/fish/conf.d/moje.fish
+++ home/.config/fish/conf.d/moje.fish
@@ -3,6 +3,7 @@ alias gd "git diff"
 alias POGODA "curl http://wttr.in/warsaw"
 alias man "env LANG=en_US man"
 alias DONG "git fetch ; git rebase"
+alias Knt "bundle exec knt"
 
 eval (thefuck --alias | tr '\n' ';')
 
