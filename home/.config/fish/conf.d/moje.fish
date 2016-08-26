61d02c7590ca8ccb6c584e3cce3cc80dad6eb752
Bartłomiej Wręga <bwrega@gmail.com>
2016-08-26 13:02:33 +0200

    git - added DONG alias

--- home/.config/fish/conf.d/moje.fish
+++ home/.config/fish/conf.d/moje.fish
@@ -2,6 +2,7 @@ alias gs "git status"
 alias gd "git diff"
 alias POGODA "curl http://wttr.in/warsaw"
 alias man "env LANG=en_US man"
+alias DONG "git fetch ; git rebase"
 
 eval (thefuck --alias | tr '\n' ';')
 
