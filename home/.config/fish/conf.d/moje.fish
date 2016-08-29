a8c6ad0109517b4af46aef2556ee38a12a3c336d
Bartłomiej Wręga <bartlomiej.wrega@kontomatik.com>
2016-08-29 12:37:02 +0200

    removed Knt alias

--- home/.config/fish/conf.d/moje.fish
+++ home/.config/fish/conf.d/moje.fish
@@ -3,7 +3,6 @@ alias gd "git diff"
 alias POGODA "curl http://wttr.in/warsaw"
 alias man "env LANG=en_US man"
 alias DONG "git fetch ; git rebase"
-alias Knt "bundle exec knt"
 
 eval (thefuck --alias | tr '\n' ';')
 
