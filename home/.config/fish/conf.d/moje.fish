7338618779b9f6e1f7a5d1512f16529da2cb59c5
Bartłomiej Wręga <bwrega@gmail.com>
2022-06-29 18:00:03 +0200

    removed outdated go from path

--- home/.config/fish/conf.d/moje.fish
+++ home/.config/fish/conf.d/moje.fish
@@ -2,12 +2,12 @@ alias gs "git status"
 alias gd "git diff"
 alias POGODA "curl http://wttr.in/warsaw"
 alias man "env LANG=en_US man"
-alias DONG "git fetch ; git rebase"
+alias DONG "git fetch --all; git rebase"
 
 #set -ex GREP_OPTIONS
 
 set -x JREBEL_HOME /home/bartek/Soft/jrebel
-set -x GOPATH /home/bartek/sandbox/go
-set -x GOROOT /home/bartek/Soft/go
+#set -x GOPATH /home/bartek/sandbox/go
+#set -x GOROOT /home/bartek/Soft/go
 
 
